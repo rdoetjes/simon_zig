d4dffbb4a51c55e5f80caa8ddc8ce314c1725303
Raymond Doetjes <rdoetjes@phonax.com>
2023-09-02 19:27:02 +0200

    comments for the final code

--- simon/simon.zig
+++ simon/simon.zig
@@ -14,6 +14,10 @@ const pin_config = rp2040.pins.GlobalConfiguration{
 
 var piezo: rp2040.pwm.Pwm(5, .a) = undefined;
 
+// setup the pins
+// 2,3,4,5 are LEDs red,green,yellow,blue
+// 6,7,8,9 are switches that correspond with LEDs, red,green,yellow,blue
+// 10 is the piezo
 fn setup() void {
     for (2..6) |r| {
         var i: u5 = @truncate(r);
@@ -34,6 +38,8 @@ fn setup() void {
     piezo.slice().disable();
 }
 
+// set the game into a known state
+// generate a random sequence for simon, for this game
 fn reset_game(sequence: *[max_sequence_size]u8) void {
     var ascon = rand.Ascon.init();
     var rng = ascon.random();
@@ -44,10 +50,14 @@ fn reset_game(sequence: *[max_sequence_size]u8) void {
     }
 }
 
+// stop the pwm to prevent the sound from playing
 fn stop_beep() void {
     piezo.slice().disable();
 }
 
+// plays a beep with the frequene corresponding to the move
+// moves are 0,1,2,3 and other value sets pwm wrap to 0
+// in affect making frequence so high that it's inaudinle
 fn play_beep(move: u8) void {
     piezo.slice().set_clk_div(4, 0);
     switch (move) {
@@ -64,6 +74,8 @@ fn play_beep(move: u8) void {
     piezo.slice().enable();
 }
 
+// simon calls this to show a move to the player with a specified duration
+// speed is the time it shows/sounds the move
 fn play_move(move: u8, speed: u32) void {
     var pin: u5 = @truncate(move + 2);
     play_beep(move);
@@ -74,38 +86,30 @@ fn play_move(move: u8, speed: u32) void {
     time.sleep_ms(speed);
 }
 
+// simon's move, simon will play through the sequence up to the current step + 1
 fn simon(sequence: *[max_sequence_size]u8, step: usize, speed: u32) void {
     for (0..step + 1) |i| {
         play_move(sequence[i], speed);
     }
 }
 
+// select level, red switch = 10 steps, green switch = 15, yellow = 20 steps (pfff), blue = 30 steps (goodluck!!!)
 fn select_level() u8 {
-    var level: u8 = 0;
-
-    while (level == 0) {
+    while (true) {
         if (gpio.num(6).read() == 0) {
-            level = 10;
+            return 10;
+        } else if (gpio.num(7).read() == 0) {
+            return 15;
+        } else if (gpio.num(8).read() == 0) {
+            return 20;
+        } else if (gpio.num(9).read() == 0) {
+            return 30;
         }
-
-        if (gpio.num(7).read() == 0) {
-            level = 15;
-        }
-
-        if (gpio.num(8).read() == 0) {
-            level = 20;
-        }
-
-        if (gpio.num(9).read() == 0) {
-            level = 30;
-        }
-
         time.sleep_ms(50); // debounces switch and saves a bit of energt
     }
-
-    return level;
 }
 
+// plays the game over, light and sound cue
 fn game_over() void {
     play_beep(4);
     for (0..10) |_| {
@@ -118,6 +122,7 @@ fn game_over() void {
     stop_beep();
 }
 
+// plays the, win light and sound cue.
 fn you_won() void {
     time.sleep_ms(300);
     for (0..10) |_| {
@@ -135,6 +140,9 @@ fn you_won() void {
     stop_beep();
 }
 
+// lights up the led and plays the sound of the corresponding pin when the key is down
+// this allows you to cheat ;) you can think when you hold down the key.
+// the first revision SIMON had this bug too.
 fn key_down(pin: u5) void {
     play_beep(pin - 6);
     gpio.num(pin - 4).toggle();
@@ -145,13 +153,21 @@ fn key_down(pin: u5) void {
     stop_beep();
 }
 
+// player's turn they have to play all the steps SIMON has just shown them.
+// if you wait more than 1500ms for the next button press return false.
+// if you enter the wrong move return false.
+// if you get the move correct within 1500ms return true
 fn player(sequence: *[max_sequence_size]u8, step: usize) bool {
     for (0..step + 1) |i| {
-        var count: u8 = 0;
+        const timeout_ms = 1500;
+        const loop_delay_ms = 50;
+        const max_loop = timeout_ms / 50;
 
-        var move: i8 = -1;
+        var count: u8 = 0; // count time debounce ms is the time out
 
-        while (count < 30 and move == -1) {
+        var move: i8 = -1; // no move yet
+
+        while (count < max_loop and move == -1) {
             if (gpio.num(6).read() == 0) {
                 key_down(6);
                 move = 0;
@@ -167,14 +183,16 @@ fn player(sequence: *[max_sequence_size]u8, step: usize) bool {
             }
 
             count += 1;
-            time.sleep_ms(50);
+            time.sleep_ms(loop_delay_ms);
         }
 
-        if (count >= 30 or sequence[i] != move) return false;
+        if (count >= max_loop or sequence[i] != move) return false;
     }
     return true;
 }
 
+// sets the game spede based on the step
+// SIMON grows impatient the further we get in the game and shows the moves shorter
 fn set_game_speed(step: usize) u32 {
     var result: u32 = 300;
     if (step < 4) {
@@ -191,6 +209,7 @@ fn set_game_speed(step: usize) u32 {
     return result;
 }
 
+// the game loop that handles GAME_RESET, SIMON, PLAYER, WIN AND LOOSE LOGIC.
 fn game_loop(sequence: *[max_sequence_size]u8) void {
     reset_game(sequence);
 
@@ -213,6 +232,7 @@ fn game_loop(sequence: *[max_sequence_size]u8) void {
     }
 }
 
+// Enrty point
 pub fn main() !void {
     var sequence: [max_sequence_size]u8 = undefined;
     const pins = pin_config.apply();
